5e1b31e8af4e6bd8b459fe3a6ee7ddada73314fe
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-10 18:50:31 -0400

    new update

--- ScrollView-ImageGalleries/ViewController.m
+++ ScrollView-ImageGalleries/ViewController.m
@@ -8,11 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-@property (nonatomic, strong) UIScrollView *sv;
-@property (nonatomic, strong) UIImageView *iv1;
-@property (nonatomic, strong) UIImageView *iv2;
-@property (nonatomic, strong) UIImageView *iv3;
+@interface ViewController () <UIScrollViewDelegate>
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIImageView *imageView1;
+@property (nonatomic, strong) UIImageView *imageView2;
+@property (nonatomic, strong) UIImageView *imageView3;
 @property (nonatomic, strong) UIPageControl *pageControl;
 
 @end
@@ -22,63 +22,60 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
-  self.sv.delegate = self;
+  self.scrollView.delegate = self;
   [self setupScrollViews];
-  self.sv.pagingEnabled = YES;
+  self.scrollView.pagingEnabled = YES;
   [self setupPageControl];
 }
 
 
 
 - (void)setupScrollViews {
-  self.sv = [UIScrollView new];
-  self.sv.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.sv];
-  [self.sv.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
-  [self.sv.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
-  [self.sv.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
-  [self.sv.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
-  self.sv.pagingEnabled = YES;
+  self.scrollView = [UIScrollView new];
+  self.scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:self.scrollView];
+  [self.scrollView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+  [self.scrollView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+  [self.scrollView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+  [self.scrollView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+  self.scrollView.pagingEnabled = YES;
   
-  self.iv1 = [UIImageView new];
-  self.iv1.image = [UIImage imageNamed:@"Lighthouse"];
-  self.iv1.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.sv addSubview:self.iv1];
-  [self.iv1.leadingAnchor constraintEqualToAnchor:self.sv.leadingAnchor].active = YES;
-  [self.iv1.topAnchor constraintEqualToAnchor:self.sv.topAnchor].active = YES;
-  [self.iv1.bottomAnchor constraintEqualToAnchor:self.sv.bottomAnchor].active = YES;
-  [self.iv1.widthAnchor constraintEqualToAnchor:self.sv.widthAnchor].active = YES;
+  self.imageView1 = [UIImageView new];
+  self.imageView1.image = [UIImage imageNamed:@"Lighthouse-zoomed.imageset"];
+  self.imageView1.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.scrollView addSubview:self.imageView1];
+  [self.imageView1.leadingAnchor constraintEqualToAnchor:self.scrollView.leadingAnchor].active = YES;
+  [self.imageView1.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+  [self.imageView1.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+  [self.imageView1.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
+  [self.imageView1.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
   
-  [self.iv1.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
+  self.imageView2 = [UIImageView new];
+  self.imageView2.image = [UIImage imageNamed:@"Lighthouse-night.imageset"];
+  self.imageView2.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.scrollView addSubview:self.imageView2];
+  [self.imageView2.leadingAnchor constraintEqualToAnchor:self.imageView1.trailingAnchor].active = YES;
+  [self.imageView2.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+  [self.imageView2.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
   
-  self.iv2 = [UIImageView new];
-  self.iv2.image = [UIImage imageNamed:@"Lighthouse-night"];
-  self.iv2.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.sv addSubview:self.iv2];
-  [self.iv2.leadingAnchor constraintEqualToAnchor:self.iv1.trailingAnchor].active = YES;
-  [self.iv2.topAnchor constraintEqualToAnchor:self.sv.topAnchor].active = YES;
-  [self.iv2.bottomAnchor constraintEqualToAnchor:self.sv.bottomAnchor].active = YES;
+  [self.imageView2.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
   
-  [self.iv2.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
-  
-  [self.iv2.widthAnchor constraintEqualToAnchor:self.sv.widthAnchor].active = YES;
-  
-//  [self.iv2.trailingAnchor constraintEqualToAnchor:self.sv.trailingAnchor].active = YES;
+  [self.imageView2.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
   
   
-  self.iv3 = [UIImageView new];
-  self.iv3.image = [UIImage imageNamed:@"Lighthouse-in-Field"];
-  self.iv3.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.sv addSubview:self.iv3];
-  [self.iv3.leadingAnchor constraintEqualToAnchor:self.iv2.trailingAnchor].active = YES;
-  [self.iv3.topAnchor constraintEqualToAnchor:self.sv.topAnchor].active = YES;
-  [self.iv3.bottomAnchor constraintEqualToAnchor:self.sv.bottomAnchor].active = YES;
+  self.imageView3 = [UIImageView new];
+  self.imageView3.image = [UIImage imageNamed:@"Lighthouse-in-Field.imageset"];
+  self.imageView3.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.scrollView addSubview:self.imageView3];
+  [self.imageView3.leadingAnchor constraintEqualToAnchor:self.imageView2.trailingAnchor].active = YES;
+  [self.imageView3.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+  [self.imageView3.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
   
-  [self.iv3.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
+  [self.imageView3.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
   
-  [self.iv3.widthAnchor constraintEqualToAnchor:self.sv.widthAnchor].active = YES;
+  [self.imageView3.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
   
-  [self.iv3.trailingAnchor constraintEqualToAnchor:self.sv.trailingAnchor].active = YES;
+  [self.imageView3.trailingAnchor constraintEqualToAnchor:self.scrollView.trailingAnchor].active = YES;
   
 }
 
@@ -88,7 +85,7 @@
   [self.view bringSubviewToFront:self.pageControl];
   self.pageControl.backgroundColor = [UIColor blackColor];
   self.pageControl.alpha = 0.5;
-  self.pageControl.numberOfPages = self.sv.subviews.count;
+  self.pageControl.numberOfPages = self.scrollView.subviews.count;
   CGRect frame = CGRectMake(0, self.view.bounds.size.height-50, self.view.bounds.size.width, 50.0);
   self.pageControl.frame = frame;
 
