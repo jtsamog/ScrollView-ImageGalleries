0d2507e6182e627dadba99aef7712a55f087cdb0
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-10 14:19:38 -0400

    Initial Commit

---
+++ ScrollView-ImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView-ImageGalleries
+//
+//  Created by Endeavour2 on 7/10/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollView-ImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView-ImageGalleries
+//
+//  Created by Endeavour2 on 7/10/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
